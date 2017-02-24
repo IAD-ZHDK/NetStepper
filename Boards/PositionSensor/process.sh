2b3df623bf8a7f10c8e55b3ac4125dc21b95aebd
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-02-24 14:19:50 +0100

    updated position sensor

--- Boards/PositionSensor/process.sh
+++ Boards/PositionSensor/process.sh
@@ -5,8 +5,5 @@ export PATH=$HOME/.gp/256dpi/bin:$PATH
 gcode strip PositionSensor.top.drill.gcode PositionSensor.top.drill.gcode
 gcode strip PositionSensor.top.etch.gcode PositionSensor.top.etch.gcode
 
-gcode offset PositionSensor.top.drill.gcode PositionSensor.top.drill.off.gcode 0 15 0
-gcode offset PositionSensor.top.etch.gcode PositionSensor.top.etch.off.gcode 0 15 0
-
-awk 'NR==5 {$0="G0 X20 Y20"} 1' PositionSensor.top.drill.off.gcode > PositionSensor.top.drill.fix.gcode
-awk 'NR==5 {$0="G0 X20 Y20"} 1' PositionSensor.top.etch.off.gcode > PositionSensor.top.etch.fix.gcode
+gcode offset PositionSensor.top.drill.gcode PositionSensor.top.drill.off.gcode 17 17 0
+gcode offset PositionSensor.top.etch.gcode PositionSensor.top.etch.off.gcode 17 17 0
