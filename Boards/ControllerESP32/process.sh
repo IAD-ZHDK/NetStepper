a395db800b9cc575a0f8ec5065bb6e9e028f0126
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-03-13 13:47:43 +0100

    finished controller for mass production

--- Boards/ControllerESP32/process.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH=$HOME/.gp/256dpi/bin:$PATH
-
-gcode strip ControllerESP32.top.drill.gcode ControllerESP32.top.drill.gcode
-gcode strip ControllerESP32.top.etch.gcode ControllerESP32.top.etch.gcode
-
-gcode offset ControllerESP32.top.drill.gcode ControllerESP32.top.drill.off.gcode 17 17 0
-gcode offset ControllerESP32.top.etch.gcode ControllerESP32.top.etch.off.gcode 17 17 0
--- Boards/PositionSensor/process.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH=$HOME/.gp/256dpi/bin:$PATH
-
-gcode strip PositionSensor.top.drill.gcode PositionSensor.top.drill.gcode
-gcode strip PositionSensor.top.etch.gcode PositionSensor.top.etch.gcode
-
-gcode offset PositionSensor.top.drill.gcode PositionSensor.top.drill.off.gcode 17 17 0
-gcode offset PositionSensor.top.etch.gcode PositionSensor.top.etch.off.gcode 17 17 0
