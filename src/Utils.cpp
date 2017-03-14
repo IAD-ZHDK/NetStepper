bea19f4aa62baa779e0be988d3e9d5725dcbde95
Joël Gähwiler <joel.gaehwiler@gmail.com>
2017-03-14 15:33:25 +0100

    swapped

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,5 +1,5 @@
 #include "Utils.h"
 
-String prefix(const char *str, const char *prefix) { return String(prefix) + String(str); }
+String prefix(const char *prefix, const char *str) { return String(prefix) + String(str); }
 
 String suffix(const char *str, const char *suffix) { return String(str) + String(suffix); }
